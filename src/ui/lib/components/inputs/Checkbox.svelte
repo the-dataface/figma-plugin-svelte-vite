ad6ac5320900923eb208cad74060fe76e8c327f3
Sawyer Click <sawyer@hey.com>
2023-06-05 11:16:38 -0500

    init checkbox

---
+++ src/ui/lib/components/inputs/Checkbox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	/** @description attributes of the element */
+	export let attributes: Record<string, string> = {};
+
+	/** @description checked value of the element */
+	export let checked: boolean | undefined = undefined;
+
+	/** @description placeholder text */
+	export let label: string = '';
+</script>
+
+<label class="">
+	<input type="checkbox" {...attributes} bind:checked on:change />
+	<span>{label}</span>
+</label>
+
+<style lang="postcss">
+	label {
+	}
+	input {
+	}
+	span {
+	}
+</style>
