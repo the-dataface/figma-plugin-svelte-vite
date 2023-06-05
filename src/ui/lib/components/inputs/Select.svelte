a5c8db53e55cdf8e9051b4465103f95986967037
Sawyer Click <sawyer@hey.com>
2023-06-05 11:16:42 -0500

    init Select

---
+++ src/ui/lib/components/inputs/Select.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	/** @description attributes of the element */
+	export let attributes: Record<string, string> = {};
+
+	/** @description value of the element */
+	export let value: string = '';
+
+	/** @description array of strings as the options */
+	export let options: string[] = [];
+
+	/** @description placeholder text */
+	export let placeholder: string = '';
+</script>
+
+<select {...attributes} bind:value on:change>
+	{#if placeholder}
+		<option value="" disabled selected hidden>{placeholder}</option>
+	{/if}
+	{#each options as option (option)}
+		<option value={option} selected={option === value}>{option}</option>
+	{/each}
+</select>
+
+<style lang="postcss">
+	select {
+	}
+</style>
