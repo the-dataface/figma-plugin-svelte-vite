70ecc53541c47c684069a8a02d396f4edd4169e2
Sawyer Click <sawyer@hey.com>
2024-05-16 12:16:42 -0500

    update vite plugin config

--- vite.config.plugin.ts
+++ vite.config.plugin.ts
@@ -1,27 +1,39 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
-import writeFigmaManifest from './scripts/write-figma-manifest';
+
 import figmaManifest from './figma.manifest';
+import writeFigmaManifest from './scripts/write-figma-manifest';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [viteSingleFile(), writeFigmaManifest()],
-	build: {
-		lib: {
-			name: figmaManifest.name,
-			entry: path.resolve(__dirname, './src/plugin/plugin.ts'),
-			fileName: 'plugin',
-			formats: ['es'],
+export default defineConfig(({ mode }) => {
+	const dev = mode === 'development';
+	return {
+		plugins: [viteSingleFile(), writeFigmaManifest()],
+		build: {
+			minify: !dev,
+			sourcemap: dev,
+			lib: {
+				name: figmaManifest.name,
+				entry: path.resolve(__dirname, './src/plugin/plugin.ts'),
+				fileName: 'plugin',
+				formats: ['es'],
+			},
+			rollupOptions: {
+				output: {
+					entryFileNames: 'index.js',
+					extend: true,
+				},
+			},
+			emptyOutDir: false,
+			outDir: path.resolve(__dirname, './dist'),
 		},
-		emptyOutDir: false,
-		outDir: path.resolve(__dirname, './dist'),
-	},
-	resolve: {
-		alias: {
-			$common: path.resolve(__dirname, './src/common'),
-			$ui: path.resolve(__dirname, './src/ui'),
-			$plugin: path.resolve(__dirname, './src/plugin'),
+		resolve: {
+			alias: {
+				$common: path.resolve(__dirname, './src/common'),
+				$ui: path.resolve(__dirname, './src/ui'),
+				$plugin: path.resolve(__dirname, './src/plugin'),
+			},
 		},
-	},
+	};
 });
